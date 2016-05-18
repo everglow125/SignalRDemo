1ba1cb439039c3c9677a5c0b2e5ef09e6a9e9e99
everglow125 <183900112@qq.com>
2016-05-18 11:49:58 +0800

    文件

---
+++ ChatTest/ChatTest/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.ClientName = "用户-" + new Random().Next(10000, 99999);
+            return View();
+        }
+
+    }
+}
---
+++ ChatTest/ChatTest/SignalR/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Vml;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatTest.SignalR
+{
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public string Hello()
+        {
+            return "hello";
+        }
+        public void SendMessag(string message)
+        {
+            Clients.Others.talk(message);
+        }
+
+        // 发送图片
+        public void SendImage(string name, IEnumerable<ImageData> images)
+        {
+            foreach (var item in images ?? Enumerable.Empty<ImageData>())
+            {
+                if (String.IsNullOrEmpty(item.Picture)) continue;
+                Clients.All.receiveImage(name, item.Picture); // 调用客户端receiveImage方法将图片进行显示
+            }
+        }
+    }
+}
